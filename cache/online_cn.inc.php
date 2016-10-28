98e71bf05a01c0ca6337e7ac6498a55c3984ec40
wendy <weiwenjuan_bj@163.com>
2016-10-28 16:31:53 +0800

    wwj

---
+++ cache/online_cn.inc.php
@@ -0,0 +1,51 @@
+<?php
+ return array (
+  0 => 
+  array (
+    'id' => '1',
+    'name' => '咨询销售',
+    'no_order' => '1',
+    'qq' => '1438199312',
+    'msn' => '',
+    'taobao' => '',
+    'alibaba' => '',
+    'skype' => '',
+    'lang' => 'cn',
+  ),
+  1 => 
+  array (
+    'id' => '2',
+    'name' => '咨询销售',
+    'no_order' => '2',
+    'qq' => '1929859793',
+    'msn' => '',
+    'taobao' => '',
+    'alibaba' => '',
+    'skype' => '',
+    'lang' => 'cn',
+  ),
+  2 => 
+  array (
+    'id' => '3',
+    'name' => '空间域名',
+    'no_order' => '3',
+    'qq' => '1420076272',
+    'msn' => '',
+    'taobao' => '',
+    'alibaba' => '',
+    'skype' => '',
+    'lang' => 'cn',
+  ),
+  3 => 
+  array (
+    'id' => '4',
+    'name' => '合作加盟',
+    'no_order' => '4',
+    'qq' => '1438199312',
+    'msn' => '',
+    'taobao' => '',
+    'alibaba' => '',
+    'skype' => '',
+    'lang' => 'cn',
+  ),
+); ?>
---
+++ config/config_db.php
@@ -0,0 +1,10 @@
+<?php
+                   /*
+                   con_db_host = "localhost"
+                   con_db_id   = "root"
+                   con_db_pass	= "cef9fea7b4"
+                   con_db_name = "metinfo"
+                   tablepre    =  "met_"
+                   db_charset  =  "utf8";
+                  */
+                  ?>
